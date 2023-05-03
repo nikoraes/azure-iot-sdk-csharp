0fcb9cf5afc2ff575b351bd4cc6502ee2ea9ed44
timtay-microsoft <timtay@microsoft.com>
2023-05-02 18:01:08 -0700

    Revert "asdf"
    This reverts commit c9348452f29a94299814a88dc87ffff54170525a.

--- e2e/Tests/iothub/service/MessageFeedbackReceiverE2ETest.cs
+++ e2e/Tests/iothub/service/MessageFeedbackReceiverE2ETest.cs
@@ -23,81 +23,80 @@ namespace Microsoft.Azure.Devices.E2ETests.IotHub.Service
 
         [TestMethod]
         [Timeout(LongRunningTestTimeoutMilliseconds)]
+        [DataRow(IotHubTransportProtocol.Tcp)]
+        [DataRow(IotHubTransportProtocol.WebSocket)]
         [TestCategory("Serial")]
-        public async Task MessageFeedbackReceiver_Operation()
+        public async Task MessageFeedbackReceiver_Operation(IotHubTransportProtocol protocol)
         {
-            foreach (IotHubTransportProtocol protocol in Enum.GetValues(typeof(IotHubTransportProtocol)))
+            // Setting up one cancellation token for the complete test flow
+            using var cts = new CancellationTokenSource(s_testTimeout);
+            CancellationToken ct = cts.Token;
+
+            var options = new IotHubServiceClientOptions
             {
-                // Setting up one cancellation token for the complete test flow
-                using var cts = new CancellationTokenSource(s_testTimeout);
-                CancellationToken ct = cts.Token;
+                Protocol = protocol,
+            };
+            using var serviceClient = new IotHubServiceClient(TestConfiguration.IotHub.ConnectionString, options);
+            await using TestDevice testDevice = await TestDevice.GetTestDeviceAsync(_devicePrefix, ct: ct).ConfigureAwait(false);
 
-                var options = new IotHubServiceClientOptions
+            try
+            {
+                // Configure the device to receive messages.
+                await using IotHubDeviceClient deviceClient = testDevice.CreateDeviceClient(new IotHubClientOptions(new IotHubClientAmqpSettings()));
+                await testDevice.OpenWithRetryAsync(ct).ConfigureAwait(false);
+
+                var c2dMessageReceived = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+                Task<MessageAcknowledgement> OnC2DMessageReceived(IncomingMessage message)
                 {
-                    Protocol = protocol,
-                };
-                using var serviceClient = new IotHubServiceClient(TestConfiguration.IotHub.ConnectionString, options);
-                await using TestDevice testDevice = await TestDevice.GetTestDeviceAsync(_devicePrefix, ct: ct).ConfigureAwait(false);
+                    c2dMessageReceived.TrySetResult(true);
+                    return Task.FromResult(MessageAcknowledgement.Complete);
+                }
+                await deviceClient.SetIncomingMessageCallbackAsync(OnC2DMessageReceived).ConfigureAwait(false);
 
+                // Configure the service client to send the message.
+                var message = new OutgoingMessage("some payload")
+                {
+                    Ack = DeliveryAcknowledgement.Full,
+                    MessageId = Guid.NewGuid().ToString(),
+                };
+                var feedbackMessageReceived = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+                serviceClient.MessageFeedback.MessageFeedbackProcessor = (FeedbackBatch feedback) =>
+                {
+                    if (feedback.Records.Any(x => x.OriginalMessageId == message.MessageId))
+                    {
+                        feedbackMessageReceived.TrySetResult(true);
+                        return Task.FromResult(AcknowledgementType.Complete);
+                    }
+
+                    // Same hub as other tests, so we don't want to complete messages that aren't meant for us.
+                    return Task.FromResult(AcknowledgementType.Abandon);
+                };
+                await serviceClient.MessageFeedback.OpenAsync().ConfigureAwait(false);
+
+                await serviceClient.Messages.OpenAsync().ConfigureAwait(false);
+                await serviceClient.Messages.SendAsync(testDevice.Device.Id, message).ConfigureAwait(false);
+
+                // Wait for the device to receive the message.
+                await c2dMessageReceived.WaitAsync(cts.Token).ConfigureAwait(false);
+
+                // Wait for the service to receive the feedback message.
+                using var cts2 = new CancellationTokenSource(TimeSpan.FromSeconds(200));
+                await feedbackMessageReceived.WaitAsync(cts2.Token).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                VerboseTestLogger.WriteLine($"Test {nameof(MessageFeedbackReceiver_Operation)} failed over {protocol} due to {ex}");
+                throw;
+            }
+            finally
+            {
                 try
                 {
-                    // Configure the device to receive messages.
-                    await using IotHubDeviceClient deviceClient = testDevice.CreateDeviceClient(new IotHubClientOptions(new IotHubClientAmqpSettings()));
-                    await testDevice.OpenWithRetryAsync(ct).ConfigureAwait(false);
-
-                    var c2dMessageReceived = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
-                    Task<MessageAcknowledgement> OnC2DMessageReceived(IncomingMessage message)
-                    {
-                        c2dMessageReceived.TrySetResult(true);
-                        return Task.FromResult(MessageAcknowledgement.Complete);
-                    }
-                    await deviceClient.SetIncomingMessageCallbackAsync(OnC2DMessageReceived).ConfigureAwait(false);
-
-                    // Configure the service client to send the message.
-                    var message = new OutgoingMessage("some payload")
-                    {
-                        Ack = DeliveryAcknowledgement.Full,
-                        MessageId = Guid.NewGuid().ToString(),
-                    };
-                    var feedbackMessageReceived = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
-                    serviceClient.MessageFeedback.MessageFeedbackProcessor = (FeedbackBatch feedback) =>
-                    {
-                        if (feedback.Records.Any(x => x.OriginalMessageId == message.MessageId))
-                        {
-                            feedbackMessageReceived.TrySetResult(true);
-                            return Task.FromResult(AcknowledgementType.Complete);
-                        }
-
-                        // Same hub as other tests, so we don't want to complete messages that aren't meant for us.
-                        return Task.FromResult(AcknowledgementType.Abandon);
-                    };
-                    await serviceClient.MessageFeedback.OpenAsync().ConfigureAwait(false);
-
-                    await serviceClient.Messages.OpenAsync().ConfigureAwait(false);
-                    await serviceClient.Messages.SendAsync(testDevice.Device.Id, message).ConfigureAwait(false);
-
-                    // Wait for the device to receive the message.
-                    await c2dMessageReceived.WaitAsync(cts.Token).ConfigureAwait(false);
-
-                    // Wait for the service to receive the feedback message.
-                    using var cts2 = new CancellationTokenSource(TimeSpan.FromSeconds(200));
-                    await feedbackMessageReceived.WaitAsync(cts2.Token).ConfigureAwait(false);
+                    await serviceClient.MessageFeedback.CloseAsync().ConfigureAwait(false);
                 }
                 catch (Exception ex)
                 {
-                    VerboseTestLogger.WriteLine($"Test {nameof(MessageFeedbackReceiver_Operation)} failed over {protocol} due to {ex}");
-                    throw;
-                }
-                finally
-                {
-                    try
-                    {
-                        await serviceClient.MessageFeedback.CloseAsync().ConfigureAwait(false);
-                    }
-                    catch (Exception ex)
-                    {
-                        VerboseTestLogger.WriteLine($"Cleanup in {nameof(MessageFeedbackReceiver_Operation)} failed over {protocol} due to {ex}");
-                    }
+                    VerboseTestLogger.WriteLine($"Cleanup in {nameof(MessageFeedbackReceiver_Operation)} failed over {protocol} due to {ex}");
                 }
             }
         }
